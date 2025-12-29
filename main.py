b1e9c15072ce8e0eb8a9de1693ee0b4e108b3ede
waleedbutt991 <164307694+waleedbutt991@users.noreply.github.com>
2025-12-29 14:42:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+computer = random.choice([1,-1,0])
+youstr= (input('Enter Choice: '))
+you_dict={'G': 1, 'S': -1, 'W': 0}
+reverse_dict={1:'Gun', -1: 'Snake', 0 : 'Water'}
+
+you = you_dict[youstr]
+
+print(f'Computer choose {reverse_dict[computer]}\nYou Choose {reverse_dict[you]}' )
+
+if (computer == you):
+    print('Draw')
+    
+
+elif (computer == 1 and you == -1):
+        print('you lost')
+
+elif (computer == 1 and you == 0):
+        print('you win')
+
+elif (computer == -1 and you == 1):
+        print('you win')
+
+elif (computer == -1 and you == 0):
+        print('you lost')
+
+elif (computer == 0 and you == 1):
+       print('You lost')
+
+elif (computer == 0 and you == -1):
+       print ('you win')
+
+else:
+       print('something wrong')
